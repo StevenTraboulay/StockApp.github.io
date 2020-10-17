2d95b14faac8deac1c9985111f18684b82de7fff
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-10-17 16:38:15 -0400

    updated readme and rough notes

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # StockApp.github.io
 StockApp Project1
+
+
+------------
+GROUP PROCESS
+-------------
+1) idea concept creation
+    a) Movie API
+    b) Stock API
+    c) Sports API
+
+2) Chose Stock API
+
+3) create skeletal framework, folders git repo and repo rules
+    a) created repo rule to protect main branch
+4) Create boilerplate/ skeleton
+5) Index creation, CSS framework selection and J script creation
+6) craeted a script for rough work which is using // for us to play with code testing and rough ideas. 
+
+
+------------------------
+index creation and styling
+-------------------------
+
+
+-------------
+Java Scripting
+-------------
+1) created display data function to return data
+    a) within function shows the stock price call
+    b) the stock name call
+    c) the growth percentage call
+    d) the opening price call
+    ***issue came up but found fix and resolution***
+2) create indiviidual information calls
+3) append the calls to div
+4) get market cap info  by multiplying current stock price by the outstanding shares
+
+
+
+
+
+-------------
+ISSUES
+-------------
+
+1) cannot pull price from the API in the way we want as it pulls the price opena and close for that moment in time.
+Fix: get the last refresh time as variable and get last refreh variable but need to get day open price
+(note: dont hardcode the query name)
+    a) need to mathematically determine which one was the earliest one in the day
+    b)get the lastrefreshed time create that variable which is pulled from API
+    c)then get current closed price variable which is returned data using lastrefreshed time
+    d)create a variable list of times data array using Object.keys
+    e) use .sort fort he listoftimes variable
+    f)create a new variable called openingTime = listofTimes[0]; which allows us to start from the begining of the array 
+    g)created a new variable to get open stock price by using open time
+    h) craeted growth and percentage variables based of open stock price and percentage
+
+
+
--- Rough work/stevensroughjava.js
+++ Rough work/stevensroughjava.js
@@ -14,44 +14,100 @@ function makeRow(searchValue) {
     var text = searchValue;
     listEl.textContent = text;
     var historyEl = document.querySelector(".stock-history");
-    //console.log(event.target);
+    console.log(event.target);
     historyEl.onclick = function () {
-      //console.log(event.target.tagName);
+      console.log(event.target.tagName);
       if (event.target.tagName == "LI") {
         searchStock(event.target.textContent)
-      }
-    };
+}
+   };
     historyEl.appendChild(listEl);
-  }
+ }
+
+      //current stock content example rough work
+      var stockNameEl = document.createElement("h3")
+      stockNameEl.classList.add("#stock-name");
+      stockName.textContent =
+        data.name + "(" + new Date().toLocaleDateString() + ")";
+      var stockBox = document.createElement("div");
+      stockBox.classList.add("XXXXXXXX");
+      var stockTickerEl = document.createElement("p");
+
+    //alternative 
+    
+
+    //-------------THE FIX ----------------------
+    //get price
+    console.log(data)
+    //gett data from API
+    var lastRefreshedTime = data['Meta Data']['3. Last Refreshed']; // required spaces IF SPACES ARE TEHRE
+
+    var dayOpenPrice = Object.keys(data['Time Series (5min)']);  // <<<--- returns an array of all the keys
+    //console.log (lostOfTimes)
+    //  sorting the data returned and default is ascending alphabetically 
+    listOfTimes.Sort()
+    console.log(listOfTimes)
+    //returning the earliest time from the list OfTimes sort in thiis case the [0] array item
+    var openingTime = listOfTimes[0];
+    console.log(openingTime);
+    //getting the opening price data from the earliest time from the array above
+    var dayOpeningPrice = data['time Series (5min)'][openingTime]['4. close'];
+    console.log(dayOpeningPrice);
+    //storting growth percentage from currentclose price and day opening price above
+    var growthPercentage = (currentClosePrice / dayOpeningPrice -1) * 100;
+    console.log(growthPercentage);
+    //storing different from current close pprice and day opening price
+    var absoluteGrowth = currentClosePrice = dayOpeningPrice;
+    console.log(absoluteGrowth);
+
+
+    //append to outer container
+    outerStockContaainerNameEl.appendchild(stockNameEl);
 
   ///-----------working with Asghar
 
 
-  var stockSubmit = document.querySelector("input-group");
-  var stockAPIkey = "EM3FI6FS0TMXXLD";
-  var ApiKey = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockInput + "&interval=5min&apikey=" + stockAPIkey;
 
 
-  var formSubmitHandler = function(event){
-      event.preventDefault();
-      var stockInput = document.querySelector("#stock-input").value.trim();
-      console.log(stockInput)
-      if(stockInput){
-          getStockData(stockInput)
-          //get
-          //save
-          var clearInput = document.querySelector("#stock-input");
-          clearInput.value="";
-    
-      }else{
-          alert ("Enter a Correct Symbol!");
-      }
+//Initiated Variables
+var stockSubmit = document.querySelector("#input-group");
+var outerStockContainerEl = document.querySelector("")
 
-  }
+//This executes when the event listener kicks off to handle the button click
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    var stockInput = document.querySelector("#stock-input").value.trim();
+    console.log(stockInput)
+    if(stockInput){
+        getStockData(stockInput)
+        
+
+           
+        var clearInput = document.querySelector("#stock-input");
+        clearInput.value = "";
+   
+    }else{
+        alert("Enter a correct Symbol!");
+    }
+}
+
+var getStockData = function(stockInput){
+    //var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=443d53b576fbee38c5cf0db4dbe2ff2b";
+    var ApiKey = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockInput + "&interval=5min&apikey=EME3FI6FSOTMXXLD";
+    fetch(ApiKey)
+    .then(function(response){
+        if(response.ok){
+            response.json()
+            .then(function(data){
+                //displayWeatherData(data, city);
+                console.log(data);
+            });
+        }else{
+            alert("Error Returned: " + response.statusText);
+        }
+    })
+};
 
 
-  var getStockaData = function (Stock){
-
-  }
-
-  stockSubmit.addEventListener("submit", formSubmitHandler);
+//On click form submit even handler
+stockSubmit.addEventListener("submit", formSubmitHandler);
