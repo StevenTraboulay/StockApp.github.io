364c3d80fbff26ed43493b6a1d576cd15d0e93c9
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-10-18 13:57:07 -0400

    cleaned up coding added comments and updated READme

--- README.md
+++ README.md
@@ -38,6 +38,9 @@ Java Scripting
 4) get market cap info  by multiplying current stock price by the outstanding shares
 5) create variables to open the openingprice, currentprice, change percentage and market cap to append into DIV
 6) append into DIV
+*** Issue came up but found fix and resolution***
+7) Maret cap issue wasnt displaying well, was able to input a new return data point to allow it to flow through
+8) fixed the percetange returns and prolonged 0.%
 
 
 
@@ -50,14 +53,24 @@ ISSUES
 1) cannot pull price from the API in the way we want as it pulls the price opena and close for that moment in time.
 Fix: get the last refresh time as variable and get last refreh variable but need to get day open price
 (note: dont hardcode the query name)
-    1)a) need to mathematically determine which one was the earliest one in the day
-    1)b)get the lastrefreshed time create that variable which is pulled from API
-    1)c)then get current closed price variable which is returned data using lastrefreshed time
-    1)d)create a variable list of times data array using Object.keys
-    1)e) use .sort fort he listoftimes variable
-    1)f)create a new variable called openingTime = listofTimes[0]; which allows us to start from the begining of the array 
-    1)g)created a new variable to get open stock price by using open time
-    1)h) craeted growth and percentage variables based of open stock price and percentage
+    1. a) need to mathematically determine which one was the earliest one in the day
+    1. b)get the lastrefreshed time create that variable which is pulled from API
+    1. c)then get current closed price variable which is returned data using lastrefreshed time
+    1. d)create a variable list of times data array using Object.keys
+    1. e) use .sort fort he listoftimes variable
+    1. f)create a new variable called openingTime = listofTimes[0]; which allows us to start from the begining of the array 
+    1. g)created a new variable to get open stock price by using open time
+    1. h) craeted growth and percentage variables based of open stock price and percentage
+2) API Problem:
+    2. a) the API is limited to 5 calls every 5 mins. and 500 calls a day.
+    2. b) the work around for this is to limit your calls. please be cognizant of this. 
+
+3) Market Cap value was not coming back with a usable value, or if it did it was too long of a number.
+    3. a) there needed to be a return object in the if statement. 
+    3. b) adding multiple points of return adds a reference point and lets us have a point for the data so it doesn't get lost. 
+
+
+
 
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,173 +1,188 @@
-//Initiated Variables
+//Initiated Global Variables
 var stockSubmit = document.querySelector("#input-group");
 var outerStockContainerEl = document.querySelector("#stock-info");
 var outerStockContainerNameEl = document.querySelector("#stock-ticker-name");
-var outerStockContainerCompanyNameEl = document.querySelector("#stock-company-name");
-var outerStockContainerOpeningPriceEl = document.querySelector("#stock-open-price");
-var outerStockContainerCurrentPriceEl = document.querySelector("#stock-last-price");
-var outerStockContainerChangePercentEl = document.querySelector("#stock-change-percent");
-var outerStockContainerAbsoluteEl = document.querySelector("#stock-change-absolute");
-var outerStockContainerMarketCapEl = document.querySelector("#stock-market-cap");
+var outerStockContainerCompanyNameEl = document.querySelector(
+  "#stock-company-name"
+);
+var outerStockContainerOpeningPriceEl = document.querySelector(
+  "#stock-open-price"
+);
+var outerStockContainerCurrentPriceEl = document.querySelector(
+  "#stock-last-price"
+);
+var outerStockContainerChangePercentEl = document.querySelector(
+  "#stock-change-percent"
+);
+var outerStockContainerAbsoluteEl = document.querySelector(
+  "#stock-change-absolute"
+);
+var outerStockContainerMarketCapEl = document.querySelector(
+  "#stock-market-cap"
+);
 
 //This executes when the event listener kicks off to handle the button click
-var formSubmitHandler = function(event){
-    event.preventDefault();
-    var stockInput = document.querySelector("#stock-input").value.trim();
-    if(stockInput){
-        clearOut();
-        getStockData(stockInput)
-        getMarketCap(stockInput)
-        var clearInput = document.querySelector("#stock-input");
-        clearInput.value = "";
-    }else{
-        alert("Enter a correct Symbol!");
-    }
-}
-
-var clearOut = function(){
-    outerStockContainerNameEl.textContent = "";
-    outerStockContainerCompanyNameEl.textContent="";
-    outerStockContainerOpeningPriceEl.textContent = "";
-    outerStockContainerCurrentPriceEl.textContent = "";
-    outerStockContainerChangePercentEl.textContent = "";
-    outerStockContainerAbsoluteEl.textContent = "";
-    outerStockContainerMarketCapEl.textContent = "";
-}
-
-var getStockData = function(stockInput){
-    var ApiKey = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockInput + "&interval=5min&apikey=EME3FI6FSOTMXXLD";
-    fetch(ApiKey)
-    .then(res => res)
-    .then(function(response){
-        if(response.ok){
-            response.json()
-            .then(function(data){
-                displayStockData(data, stockInput);
-            });
-        }else{
-            alert("No Stock Data Returned: " + response.statusText);
-        }
-    })
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var stockInput = document.querySelector("#stock-input").value.trim();
+  if (stockInput) {
+    clearOut();
+    getStockData(stockInput);
+    getMarketCap(stockInput);
+    var clearInput = document.querySelector("#stock-input");
+    clearInput.value = "";
+  } else {
+    alert("Enter a correct Symbol!");
+  }
+};
+//clear containers
+var clearOut = function () {
+  outerStockContainerNameEl.textContent = "";
+  outerStockContainerCompanyNameEl.textContent = "";
+  outerStockContainerOpeningPriceEl.textContent = "";
+  outerStockContainerCurrentPriceEl.textContent = "";
+  outerStockContainerChangePercentEl.textContent = "";
+  outerStockContainerAbsoluteEl.textContent = "";
+  outerStockContainerMarketCapEl.textContent = "";
 };
 
-var getMarketCap = function(stockInput){
-    var ApiKey = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" + stockInput + "&apikey=EME3FI6FSOTMXXLD";
-    fetch(ApiKey)
-    .then(res => res)
-    .then(function(response){
-        if(response.ok){
-            response.json()
-            .then(function(data){
-                displayMarketCap(data);    
-            });
-        }else{
-            alert("No Market Cap Returned " + response.statusText);
-        }
-    })
+//fetch call for stockdata from API
+var getStockData = function (stockInput) {
+  var ApiKey =
+    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+    stockInput +
+    "&interval=5min&apikey=EME3FI6FSOTMXXLD";
+  fetch(ApiKey)
+    .then((res) => res)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayStockData(data, stockInput);
+        });
+      } else {
+        alert("No Stock Data Returned: " + response.statusText);
+      }
+    });
+};
+//fetch call for marketcap data from API
+var getMarketCap = function (stockInput) {
+  var ApiKey =
+    "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" +
+    stockInput +
+    "&apikey=EME3FI6FSOTMXXLD";
+  fetch(ApiKey)
+    .then((res) => res)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayMarketCap(data);
+        });
+      } else {
+        alert("No Market Cap Returned " + response.statusText);
+      }
+    });
 };
 
+var displayStockData = function (data, stockInput) {
+  console.log(data);
 
-var displayStockData = function(data, stockInput){
-    console.log(data)
+  //check if api returned any repos
+  if (data.length === 0) {
+    outerStockContainerEl.textContent = "No repos found, try again";
+    return;
+  }
+  //get symbol name
+  var stockName = stockInput;
+  var StockNameEl = document.createElement("div");
+  StockNameEl.textContent = "Ticker: " + stockName.toUpperCase();
 
-    //check if api returned any repos
-    if(data.length === 0){
-        outerStockContainerEl.textContent = "No repos found, try again";
-        return;
+  //get price starts here
+  //getting this from the returned API
+  var lastRefreshedTime = data["Meta Data"]["3. Last Refreshed"]; //this has spaces so we use [] to get to data
+
+  //getting this from returned data using lastRefreshedTime
+  var currentClosePrice =
+    data["Time Series (5min)"][lastRefreshedTime]["4. close"];
+  currentClosePrice = parseFloat(currentClosePrice).toFixed(2);
+  var currentClosePriceEl = document.createElement("div");
+  currentClosePriceEl.textContent = "Current Price: $" + currentClosePrice;
+
+  //Objects.keys returns an array of all the keys from the data [time Series]
+  var listOfTimes = Object.keys(data["Time Series (5min)"]);
+
+  //sorting the data returned and the default is asc alphabitcally
+  listOfTimes.sort();
+
+  //returning the earliest time from the listOfTimes sort in this case the [0] array item
+  var openingTime = listOfTimes[0];
+
+  //getting the opening time using the opening time from the array above
+  var dayOpeningPrice = data["Time Series (5min)"][openingTime]["4. close"];
+  var dayOpeningPrice = parseFloat(dayOpeningPrice).toFixed(2);
+  var dayOpeningPriceEl = document.createElement("div");
+  dayOpeningPriceEl.textContent = "Opening Day Price: $" + dayOpeningPrice;
+
+  //calculations start here
+  //storing the percentage growth from the currentClosePrice and the dayOpeningPrice from above
+  var growthPercentage = (currentClosePrice / dayOpeningPrice - 1) * 100;
+  growthPercentage = parseFloat(growthPercentage).toFixed(2);
+  var growthPercentageEl = document.createElement("div");
+  growthPercentageEl.textContent =
+    "Change Percentage: " + growthPercentage + "%";
+
+  //storing the difference from currentClosePricee and dayOpeningPrice
+  var absoluteGrowth = currentClosePrice - dayOpeningPrice;
+  absoluteGrowth = parseFloat(absoluteGrowth).toFixed(2);
+  var absoluteGrowthEl = document.createElement("div");
+  if (absoluteGrowth < 1) {
+    absoluteGrowthEl.textContent = "Change: " + absoluteGrowth + " cents";
+  } else {
+    absoluteGrowthEl.textContent = "Change: $" + absoluteGrowth;
+  }
+
+  //append to outer container
+  outerStockContainerNameEl.appendChild(StockNameEl);
+  outerStockContainerOpeningPriceEl.appendChild(dayOpeningPriceEl);
+  outerStockContainerCurrentPriceEl.appendChild(currentClosePriceEl);
+  outerStockContainerChangePercentEl.appendChild(growthPercentageEl);
+  outerStockContainerAbsoluteEl.appendChild(absoluteGrowthEl);
+
+  //outer container big one
+  outerStockContainerEl.appendChild(outerStockContainerNameEl);
+  outerStockContainerEl.appendChild(outerStockContainerOpeningPriceEl);
+  outerStockContainerEl.appendChild(outerStockContainerCurrentPriceEl);
+  outerStockContainerEl.appendChild(outerStockContainerChangePercentEl);
+  outerStockContainerEl.appendChild(outerStockContainerAbsoluteEl);
+  outerStockContainerEl.appendChild(outerStockContainerMarketCapEl);
+};
+// function to pull and display the market capitalization information
+var displayMarketCap = function (data) {
+  var marketCap = data.MarketCapitalization;
+  var companyName = data["Name"];
+  // Could add in things like description, PE, exchange, others from here
+
+  // Recursive loop for determining the post-fix for market cap according to the stock's valuation.
+  var marketCapIterate = function (data, counter) {
+    data = parseInt(data);
+    var testVal = data / 1000;
+    if (testVal > 999) {
+      return marketCapIterate(data / 1000, counter + 1);
+    } else {
+      caps = { 0: " Thousand", 1: " Million", 2: " Billion", 3: " Trillion" };
+      var returnVal = testVal.toString() + caps[counter];
+      return returnVal.toString();
     }
-        //get symbol name
-        var stockName = stockInput;
-        var StockNameEl = document.createElement("div");
-        StockNameEl.textContent = "Ticker: " + stockName.toUpperCase();
+  };
 
-        //get price starts here
-        //getting this from the returned API
-        var lastRefreshedTime = data['Meta Data']['3. Last Refreshed']; //this has spaces so we use [] to get to data
-
-        //getting this from returned data using lastRefreshedTime
-        var currentClosePrice = data['Time Series (5min)'][lastRefreshedTime]['4. close'];
-        currentClosePrice = parseFloat(currentClosePrice).toFixed(2);
-        var currentClosePriceEl = document.createElement("div");
-        currentClosePriceEl.textContent = "Current Price: $" + currentClosePrice;
-
-        //Objects.keys returns an array of all the keys from the data [time Series]
-        var listOfTimes = Object.keys(data['Time Series (5min)']);
-
-        //sorting the data returned and the default is asc alphabitcally
-        listOfTimes.sort()
-   
-        //returning the earliest time from the listOfTimes sort in this case the [0] array item
-        var openingTime = listOfTimes[0];
-    
-        //getting the opening time using the opening time from the array above
-        var dayOpeningPrice = data['Time Series (5min)'][openingTime]['4. close'];
-        var dayOpeningPrice = parseFloat(dayOpeningPrice).toFixed(2);
-        var dayOpeningPriceEl = document.createElement("div");
-        dayOpeningPriceEl.textContent = "Opening Day Price: $" + dayOpeningPrice;
-       
-        //calculations start here
-        //storing the percentage growth from the currentClosePrice and the dayOpeningPrice from above
-        var growthPercentage = (currentClosePrice / dayOpeningPrice -1) * 100;
-        growthPercentage = parseFloat(growthPercentage).toFixed(2);
-        var growthPercentageEl = document.createElement("div");
-        growthPercentageEl.textContent = "Change Percentage: " + growthPercentage + "%";
-
-        //storing the difference from currentClosePricee and dayOpeningPrice
-        var absoluteGrowth = currentClosePrice - dayOpeningPrice;
-        absoluteGrowth = parseFloat(absoluteGrowth).toFixed(2);
-        var absoluteGrowthEl = document.createElement("div");
-        if(absoluteGrowth < 1){
-            absoluteGrowthEl.textContent = "Change: " + absoluteGrowth + " cents";
-        }else{
-            absoluteGrowthEl.textContent = "Change: $" + absoluteGrowth;
-        }
-
-        //append to outer container
-        outerStockContainerNameEl.appendChild(StockNameEl);
-        outerStockContainerOpeningPriceEl.appendChild(dayOpeningPriceEl);
-        outerStockContainerCurrentPriceEl.appendChild(currentClosePriceEl);
-        outerStockContainerChangePercentEl.appendChild(growthPercentageEl);
-        outerStockContainerAbsoluteEl.appendChild(absoluteGrowthEl);
-
-        //outer container big one
-        outerStockContainerEl.appendChild(outerStockContainerNameEl);
-        outerStockContainerEl.appendChild(outerStockContainerOpeningPriceEl);
-        outerStockContainerEl.appendChild(outerStockContainerCurrentPriceEl);
-        outerStockContainerEl.appendChild(outerStockContainerChangePercentEl);
-        outerStockContainerEl.appendChild(outerStockContainerAbsoluteEl);
-        outerStockContainerEl.appendChild(outerStockContainerMarketCapEl);
-}
-
-var displayMarketCap = function(data){
-    var marketCap = data.MarketCapitalization;
-    var companyName = data['Name'];
-    // Could add in things like description, PE, exchange, others from here
-
-
-    // Recursive loop for determining the post-fix for market cap according to the stock's valuation.
-    var marketCapIterate = function(data, counter) {
-        data = parseInt(data);
-        var testVal = data/1000
-        if (testVal > 999) {
-            return marketCapIterate(data/1000, counter+1)
-        }
-        else {
-            caps = {0:' Thousand', 1:' Million', 2:' Billion', 3:' Trillion'}
-            var returnVal = testVal.toString()+caps[counter];
-            return (returnVal.toString())
-        }
-    }
-
-    var marketCapFormatted = marketCapIterate(marketCap, 0);
-    var companyNameEl = document.createElement("div").innerHTML = '<h2>'+companyName+'</h2>'
-    var marketCapEl = document.createElement("div");
-    marketCapEl.textContent = "Market Cap: $ " + marketCapFormatted;
-    outerStockContainerEl.innerHTML = companyNameEl+outerStockContainerEl.innerHTML;
-    outerStockContainerMarketCapEl.appendChild(marketCapEl);
-}
-
+  var marketCapFormatted = marketCapIterate(marketCap, 0);
+  var companyNameEl = (document.createElement("div").innerHTML =
+    "<h2>" + companyName + "</h2>");
+  var marketCapEl = document.createElement("div");
+  marketCapEl.textContent = "Market Cap: $ " + marketCapFormatted;
+  outerStockContainerEl.innerHTML =
+    companyNameEl + outerStockContainerEl.innerHTML;
+  outerStockContainerMarketCapEl.appendChild(marketCapEl);
+};
 
 //On click form submit even handler
 stockSubmit.addEventListener("submit", formSubmitHandler);
-
-
